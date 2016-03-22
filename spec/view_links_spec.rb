0b91c4e25d013c4a9fef605c52e35d998e8ce7f0
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-22 12:43:12 +0000

    Created Links erb

--- spec/view_links_spec.rb
+++ spec/view_links_spec.rb
@@ -4,8 +4,6 @@ feature "View links" do
   scenario "see a list of links on the links page" do
     Link.create(title: "Makers Academy", url: "http://www.makersacademy.com/" )
     visit "/links"
-    within "ul#links" do
-      expect(page).to have_content("Makers Academy")
-    end
+    expect(page).to have_link("Makers Academy")
   end
 end

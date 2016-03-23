37a410ab9ed2e53e01a373e96f821531cf2203e6
olmesm <olmesm@gmail.com>
2016-03-23 14:55:34 +0000

    changed feature tests to accommmodate tags'

---
+++ app/models/tags.rb
@@ -0,0 +1,8 @@
+require 'helpers/data_mapper_setup'
+
+class Tag
+  include DataMapper::Resource
+
+  property :id,  Serial
+  property :name, String
+end
--- spec/features/adding_links_spec.rb
+++ spec/features/adding_links_spec.rb
@@ -1,13 +1,22 @@
 feature 'adding links' do
-  scenario 'allows user to add a new link' do
+
+  before do
     visit 'links/new'
     fill_in('url', with: 'http://google.com')
     fill_in('title', with: 'Google')
+    fill_in('tag', with: 'search engine')
     click_button 'Add link'
-    expect(current_path).to eq '/links'
+  end
 
+  scenario 'allows user to add a new link' do
+    expect(current_path).to eq '/links'
     within 'ul#links' do
         expect(page).to have_content('Google')
       end
+  end
+
+  xscenario 'tags can be added' do
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('education')
   end
 end
